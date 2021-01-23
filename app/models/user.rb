8875f6e18117d718d77923426b4663d46b56101f
Nock32 <daisuke.abe.wave@gmail.com>
2021-01-23 10:35:11 +0900

    validation_definition

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :prototypes
+  has_many :comments
+
+  validates :nickname,         presence: true
+  validates :email,            presence: true , unique: true
+  validates :password,         presence: true
+  validates :family_nama,      presence: true
+  validates :first_name,       presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana,  presence: true
+  validates :birthday,         presence: true
 end
